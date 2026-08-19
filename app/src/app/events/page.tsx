d46b7250a0e2d7656c65dd1d55d35f5178c6d3e0
phoubus <shungless@hotmail.com>
2026-08-19 22:34:47 +0100

    evetns thing set up code wise

--- app/src/app/events/page.tsx
+++ app/src/app/events/page.tsx
@@ -1,7 +1,9 @@
-import { requireRole } from "@/lib/supabase/GetAuth";
+import { requireRole } from "@l/supabase/GetAuth";
 
-import DividerLine from "@/Components/ui/DividerLine";
-
+import DividerLine from "@c/ui/DividerLine";
+import ListEventCard from "./ListEventCard";
+import GetEvents from "@/lib/supabase/GetEvents";
+import { type Event } from "@t/Events";
 const months = [
   "January",
   "February",
@@ -17,16 +19,53 @@ const months = [
   "December",
 ] as const;
 
+type Month = (typeof months)[number];
+
+type DisplayEvent = {
+  month: Month;
+  events: Event[];
+};
+
 async function Events() {
   await requireRole();
+  const events = await GetEvents();
+  if (!events) {
+    console.log("no events");
+    return;
+  }
+  // 2026-08-19T15:02:17.000Z
+  var months_in: Month[] = [];
+
+  var displayEvents: DisplayEvent[] = [];
+  events.forEach((event) => {
+    const date = new Date(event.date);
+    const month = months[date.getMonth()];
+    if (months_in.includes(month)) {
+      displayEvents[displayEvents.length - 1].events.push(event);
+    } else {
+      displayEvents.push({ month: month, events: [event] });
+    }
+  });
+
+  console.log("output:", JSON.stringify(displayEvents, null, 2));
   return (
     <div className="flex flex-col p-10">
-      {months.map((month) => (
-        <div key={month}>
-        <h2 className="text-3xl text-text-primary font-bold mb-2">
-          {month}
-        </h2>
-        <DividerLine orientation="horizontal" />
+      <pre className="text-black">{JSON.stringify(displayEvents, null, 2)}</pre>
+      {displayEvents.map((eventMonth, index) => (
+        <div key={index}>
+          <h2 className="text-3xl text-text-primary font-bold mb-2">
+            {eventMonth.month}
+          </h2>
+          <DividerLine orientation="horizontal" />
+          {eventMonth.events.map((value, index) => {
+            return (
+              <ListEventCard
+                key={index}
+                title={value.title}
+                description={value.description}
+                link=""></ListEventCard>
+            );
+          })}
         </div>
       ))}
     </div>
