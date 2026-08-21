534a54ffddb4ca352f42fd1390ac996286576056
GrayHound85 <oliver.norrie@gmail.com>
2026-08-21 14:13:13 +0100

    Updated Home page: changed colours and spacing of main element

--- app/src/Components/ui/HomeButton.tsx
+++ app/src/Components/ui/HomeButton.tsx
@@ -5,7 +5,7 @@ export default function HomeButton() {
     <Link href={"/"} className="hover:bg-blue-50 rounded-sm">
       <div className="flex-col text-l">
         <div className="flex gap-1">
-          <h1 className="text-text-primary font-bold tracking-wide">STEM</h1>
+          <h1 className="text-slate-700 font-bold tracking-wide">STEM</h1>
           <h1 className="text-blue-800 font-semibold tracking-wide">
             Scotland
           </h1>
--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -12,10 +12,13 @@ export default async function Home() {
         bg-[radial-gradient(circle_at_85%_90%,blue_0%,white_50%)]"
     >
       <div className="flex flex-col justify-center items-center w-1/2 h-full">
-        <div className="w-3/4">
-          <h1 className="text-6xl">Discover</h1>
-          <h1 className="text-6xl">Scotland's STEM</h1>
-          <h1 className="text-6xl mb-4">Opportunities</h1>
+        <div className="w-3/4 flex flex-col gap-5">
+          <div>
+            <h1 className="text-6xl text-slate-700">Discover</h1>
+            <h1 className="text-6xl text-primary-hover">Scotland's STEM</h1>
+            <h1 className="text-6xl mb-4 text-slate-700">Opportunities</h1>
+          </div>
+
           <p className="mb-6">
             The central hub for scottish teachers, students, volunteers and
             event organisers to discover opportunities, get critical information
